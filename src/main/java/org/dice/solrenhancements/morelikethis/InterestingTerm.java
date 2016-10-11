58639a81ba5be41f5d84b41846a725a9bb899d4f
Jingwen Wang <jingwen.w2010@gmail.com>
2016-10-11 11:02:59 -0400

    6.2.1

--- src/main/java/org/dice/solrenhancements/morelikethis/InterestingTerm.java
+++ src/main/java/org/dice/solrenhancements/morelikethis/InterestingTerm.java
@@ -13,7 +13,7 @@ public class InterestingTerm
     public float boost;
 
     public static Comparator<InterestingTerm> BOOST_ORDER = new Comparator<InterestingTerm>() {
-        @Override
+
         public int compare(InterestingTerm t1, InterestingTerm t2) {
             float d = t1.boost - t2.boost;
             if( d == 0 ) {
